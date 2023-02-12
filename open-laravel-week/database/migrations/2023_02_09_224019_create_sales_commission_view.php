d13e38e1d1af1e27315f112b59893d86a7214181
jonny <joao.vitordeoliveira@hotmail.com>
2023-02-12 12:47:04 -0400

    Criando model Sales Commission

--- open-laravel-week/database/migrations/2023_02_09_224019_create_sales_commission_view.php
+++ open-laravel-week/database/migrations/2023_02_09_224019_create_sales_commission_view.php
@@ -30,7 +30,7 @@ return new class extends Migration
                 s.sold_at,
                 s.status,
                 s.total_amount,
-                round(s.total_amount *  cp.commission_rate / 100) as commission
+                ROUND(s.total_amount * cp.commission_rate / 100) as commission
             ")->toSql();
         DB::statement("CREATE MATERIALIZED VIEW sales_commission_view AS $query");
     }
