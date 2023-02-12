e0a4913dac1ed3be718eb4ad0318038de67ed4ed
jonny <joao.vitordeoliveira@hotmail.com>
2023-02-12 15:05:30 -0400

    Construindo Controller e views para clientes, e instalando o breeze

---
+++ open-laravel-week/app/Http/Controllers/ClientController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreClientRequest;
+use App\Http\Requests\UpdateClientRequest;
+use App\Models\Client;
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function index()
+    {
+        $clients = Client::with('user', 'address')->paginate(10);
+
+        return view('clients.index', compact('clients'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Application|Factory|View
+     */
+    public function create()
+    {
+        return view('clients.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreClientRequest $request
+     * @return RedirectResponse
+     */
+    public function store(StoreClientRequest $request)
+    {
+        DB::transaction(function () use($request) {
+            $user = User::create([
+                'email' => $request->get('email'),
+                'name' =>  $request->get('name'),
+                'password'  => Hash::make('123456'),
+            ]);
+
+            $user->client()->create([
+                'address_id' => $request->get('address_id')
+            ]);
+
+        });
+
+        return redirect()->route('clients.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Client $client
+     * @return
+     */
+    public function show(Client $client)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Client $client
+     * @return Application|Factory|View
+     */
+    public function edit(Client $client)
+    {
+        return view('clients.edit', compact('client'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateClientRequest $request
+     * @param Client $client
+     * @return RedirectResponse
+     */
+    public function update(UpdateClientRequest $request, Client $client)
+    {
+        DB::transaction(function () use($request, $client) {
+            $client->user->update([
+                'email' => $request->get('email'),
+                'name' =>  $request->get('name'),
+                'password'  => Hash::make('123456'),
+            ]);
+
+            $client->update([
+                'address_id' => $request->get('address_id')
+            ]);
+
+        });
+
+        return redirect()->route('clients.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Client $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        //
+    }
+}
